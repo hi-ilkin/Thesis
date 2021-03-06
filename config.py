b0effad17fb49c805fdb5cb38574246e5f75fc51
Ilkin <ilkin@piktiv.se>
2021-03-06 23:14:34 +0100

    comparing fake vs real notebook added

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
-VIDEO_PATH = r'E:\Thesis\Thesis\data\dfdc_train_part_48\*.mp4'
-METADATA_PATH = r'E:\Thesis\Thesis\data\dfdc_train_part_48\metadata.json'
+VIDEO_PATH = r'D:\DFDC\dfdc_train_part_48\*.mp4'
+METADATA_PATH = r'D:\DFDC\dfdc_train_part_48\metadata.json'
 
 FACE_COORDINATES_PATH = 'data/face_coordinates_48.json'
-FACE_IMAGES = 'faces_48'
-DIR_CLUSTERS = 'clusters_48'
+FACE_IMAGES = 'outputs/faces_48'
+DIR_CLUSTERS = 'outputs/clusters_48'
 
-DIR_COMPARED_FACES = 'compare_48'
+DIR_COMPARED_FACES = 'outputs/compare_48'
 
 if not os.path.exists(DIR_CLUSTERS):
     os.makedirs(DIR_CLUSTERS)
