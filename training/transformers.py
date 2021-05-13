a76d4ccb73b1d8c91738093c7c76e897b20d0352
ilkin <ilkin@piktiv.se>
2021-05-13 12:38:35 +0200

    Increasing blur probability, sharpening added

--- training/transformers.py
+++ training/transformers.py
@@ -1,7 +1,8 @@
 import cv2
 
 from albumentations import Compose, RandomBrightnessContrast, HorizontalFlip, HueSaturationValue, OneOf, ToGray, \
-    ShiftScaleRotate, ImageCompression, PadIfNeeded, GaussNoise, GaussianBlur, Normalize, LongestMaxSize, CoarseDropout
+    ShiftScaleRotate, ImageCompression, PadIfNeeded, GaussNoise, GaussianBlur, Normalize, LongestMaxSize, CoarseDropout, \
+    GridDropout, CenterCrop, IAASharpen
 from albumentations.pytorch import ToTensorV2
 
 
@@ -9,12 +10,12 @@ def get_transformer(mode, size=224):
     if mode == 'train':
         return Compose([
             LongestMaxSize(size),
-            ImageCompression(quality_lower=60, quality_upper=100, p=0.5),
+            # ImageCompression(quality_lower=60, quality_upper=100, p=0.5),
             GaussNoise(p=0.1),
-            GaussianBlur(blur_limit=3, p=0.05),
+            OneOf([GaussianBlur(), IAASharpen()], p=0.5),
             HorizontalFlip(),
             # TODO: IsotropicResize
-            CoarseDropout(max_holes=1, max_width=32, max_height=32),
+            # CoarseDropout(max_holes=1, max_width=32, max_height=32),
             PadIfNeeded(min_height=size, min_width=size, border_mode=cv2.BORDER_CONSTANT),
             OneOf([RandomBrightnessContrast(), HueSaturationValue()], p=0.7),  # FancyPCA() is missing
             ToGray(p=0.2),
@@ -30,6 +31,7 @@ def get_transformer(mode, size=224):
         return Compose([
             LongestMaxSize(size),
             PadIfNeeded(min_height=size, min_width=size, border_mode=cv2.BORDER_CONSTANT),
+            # CenterCrop(height=size, width=size),
             Normalize(
                 mean=[0.485, 0.456, 0.406],
                 std=[0.229, 0.224, 0.225]
