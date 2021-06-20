94469b626c9edeae334c1efee9b23fbc674cac6d
ilkin <ilkin@piktiv.se>
2021-06-20 11:33:21 +0200

    Augmentation order were changed

--- training/transformers.py
+++ training/transformers.py
@@ -6,21 +6,21 @@ from albumentations import Compose, RandomBrightnessContrast, HorizontalFlip, Hu
 from albumentations.pytorch import ToTensorV2
 
 
-def get_transformer(mode, size=224):
+def get_transformer(mode, size=240):
     if mode == 'train':
         return Compose([
             LongestMaxSize(size),
-            # ImageCompression(quality_lower=60, quality_upper=100, p=0.5),
+            ImageCompression(quality_lower=50, quality_upper=100, p=0.5),
             GaussNoise(p=0.1),
             OneOf([GaussianBlur(), IAASharpen()], p=0.5),
             HorizontalFlip(),
-            # TODO: IsotropicResize
-            # CoarseDropout(max_holes=1, max_width=32, max_height=32),
-            PadIfNeeded(min_height=size, min_width=size, border_mode=cv2.BORDER_CONSTANT),
             OneOf([RandomBrightnessContrast(), HueSaturationValue()], p=0.7),  # FancyPCA() is missing
             ToGray(p=0.2),
-            ShiftScaleRotate(shift_limit=0.1, scale_limit=0.2, rotate_limit=10, border_mode=cv2.BORDER_CONSTANT,
-                             p=0.5),
+            ShiftScaleRotate(shift_limit=0.1, scale_limit=0.2, rotate_limit=10,
+                             border_mode=cv2.BORDER_CONSTANT, p=0.5),
+            # CoarseDropout(min_holes=2, max_holes=2, max_width=64, max_height=64),
+            # GridDropout(holes_number_x=5, holes_number_y=5, random_offset=True, p=0.5),
+            PadIfNeeded(min_height=size, min_width=size, border_mode=cv2.BORDER_CONSTANT),
             Normalize(
                 mean=[0.485, 0.456, 0.406],
                 std=[0.229, 0.224, 0.225]
